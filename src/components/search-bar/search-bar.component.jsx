6b025de041a9ebdc2bf867d53dc0ec7c68f561e5
yooifeng@yahoo.com <yooifeng@yahoo.com>
2021-03-09 00:13:38 +0800

    Updates

--- src/components/search-bar/search-bar.component.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './search-bar.styles.css';
-
-export const SearchBox =({placeholder, handler }) => (
-<input className='search' type='search' placeholder={placeholder} onChange={handler} />
-);
